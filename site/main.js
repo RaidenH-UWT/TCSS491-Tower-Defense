594a25e4200f8b090af339ac0ad59dd3d35566be
DigitalKevin <kevinhoang670@gmail.com>
2026-01-22 17:41:15 -0800

    Added a hardcoded map for basic enemy and movement

--- site/main.js
+++ site/main.js
@@ -1,12 +1,16 @@
-const gameEngine = new GameEngine();
+import { GameController } from "./controller/GameController.js";
 
+const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
+	const canvas = document.getElementById("gameCanvas");
 	const ctx = canvas.getContext("2d");
 
 	gameEngine.init(ctx);
+
+	const gameController = new GameController(gameEngine, ASSET_MANAGER);
+	gameEngine.addEntity(gameController);
 
 	gameEngine.start();
 });
